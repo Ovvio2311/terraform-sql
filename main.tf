21a5babb15bfe574322dbd447b59553b5c338934
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-08 17:16:32 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,8 @@ provider "kubernetes" {
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
-    host                   = "https://${module.gke.endpoint}"
+    # host                   = "https://${module.gke.endpoint}"
+    host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
     token                  = data.google_client_config.default.access_token
     # cluster_ca_certificate   = base64decode(module.gke.ca_certificate)
     cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
