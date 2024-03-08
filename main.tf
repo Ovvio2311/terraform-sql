31ea61e61bc7d91bc6f08a51fb048b3616faa4ef
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-09 01:14:17 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ data "google_client_config" "default" {
 data "google_container_cluster" "my_cluster" {
   name     = var.cluster_name
   location = "us-central1"
+  depends_on = [module.gke]
 }
 provider "google" {
   # credentials = file("/mnt/c/Users/jackyli/Downloads/able-scope-413414-d1f3a6012760.json")
