1e590f404c6f7786a337c074158565cb41df739f
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-10 19:42:16 +0800

    Update output.tf

--- output.tf
+++ output.tf
@@ -46,6 +46,6 @@ output "peering_name" {
   value       = module.gke.peering_name
 }
 output "project" {
-  value = data.google_client_config.default
+  value = nonsentive(data.google_client_config.default)
   sensitive = false
 }
