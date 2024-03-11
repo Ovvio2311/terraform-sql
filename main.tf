8761dedea963aa33414ca801ff3da434324f981c
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-11 11:59:52 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ provider "helm" {
       command     = "gcloud"
     }
     cluster_ca_certificate = base64decode(data.google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
-    # client_key             = base64decode(google_container_cluster.primary.master_auth.0.client_key)
+    client_key             = base64decode(data.google_container_cluster.primary.master_auth.0.client_key)
     # cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   }
 }
