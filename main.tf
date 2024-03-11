52003b1e0fa0a825802ae5b7d22d843d6590b98e
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-11 12:19:07 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,8 +52,10 @@ module "gke_auth" {
   source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
  # depends_on   = [module.gke]
   project_id   = var.project_id
-  location     = module.gke.location
-  cluster_name = module.gke.name
+  location = "us-central1-c"
+  # location     = module.gke.location
+  # cluster_name = module.gke.name
+  cluster_name = var.cluster_name
   
 }
 resource "local_file" "kubeconfig" {
