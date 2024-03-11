0528bd0b9808af900e076e78149d490c90a30a67
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-11 10:53:50 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -47,7 +47,7 @@ provider "helm" {
     # cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   }
 }
-----------------------------------------------------------------------------------------
+# ----------------------------------------------------------------------------------------
 module "gke_auth" {
   source       = "terraform-google-modules/kubernetes-engine/google//modules/auth"
   depends_on   = [module.gke]
