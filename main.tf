3e33e3be754871bf76a2d6d8da94706896816be0
Ovvio2311 <93138661+Ovvio2311@users.noreply.github.com>
2024-03-14 11:42:22 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -42,10 +42,10 @@ resource "google_compute_global_address" "private_ip_address" {
   purpose       = "VPC_PEERING"
   address_type  = "INTERNAL"
   prefix_length = 16
-  network       = "default"
+  network       = "projects/able-scope-413414/global/networks/default"
 }
 resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = "default"
+  network                 = "projects/able-scope-413414/global/networks/default"
   service                 = "servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
 }
